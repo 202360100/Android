0f10cd391cb0e11156ceed85d8e176647aac2178
202360100 <202360100@ucc.mx>
2026-08-20 20:46:08 -0600

    SectionList y FlatList

--- Ejercicio_03/App.js
+++ Ejercicio_03/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ImagenFondo from './componentes/ImagenFondo';
+import DemoFlatList from './componentes/DemoFlatList'
+import DemoSectionList from './componentes/DemoSectionList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImagenFondo/>
+      <DemoSectionList/>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
