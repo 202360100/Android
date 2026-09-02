ae21468325f1239db51e86ee1f8d439ef4bc7264
202360100 <202360100@ucc.mx>
2026-09-01 20:42:58 -0600

    Actividad en clase número 5 StackNavigation

---
+++ Parcial/StackNavigation/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import ImcScreen from './src/screens/ImcScreen';
+import CurrencyScreen from './src/screens/CurrencyScreen';
+import TipScreen from './src/screens/TipScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Home">
+        <Stack.Screen name = "Home" component = {HomeScreen} options = {{title: 'Menú Principal'}} />
+        <Stack.Screen name = "IMC" component = {ImcScreen} options = {{title: 'Calculadora IMC'}} />
+        <Stack.Screen name = "Currency" component = {CurrencyScreen} options = {{title: 'Conversor Divisas'}} />
+        <Stack.Screen name = "Tip" component = {TipScreen} options = {{title: 'Cálculo Propina'}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
