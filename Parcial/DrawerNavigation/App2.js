d2f09164012ce3722eb6da912d95525dde0bac8b
202360100 <202360100@ucc.mx>
2026-09-03 21:04:55 -0600

    Segundos cambios

--- Parcial/DrawerNavigation/App2.js
+++ Parcial/DrawerNavigation/App2.js
@@ -15,30 +15,58 @@ function SplashScreen() {
             </Text>
         </View>
     );
-
-    function HomeScreen() {
-        return(
-            <View style = {styles.home}>
-                <Text style = {styles.homeText}>
-                    ¡Bienvenido!
-                </Text>
-            </View>
-        );
-    };
-
-    export default function App2() {
-        const [loading, setLoading] = useState(true);
-        useEffect(() => {
-            setTimeout(() => {
-                setLoading(false);
-            }, 5000)
-        }, []);
-
-        if (loading) {
-            return <SplashScreen />;
-        }
-        return <HomeScreen />
-    }
-
-    
 }
+
+
+function HomeScreen() {
+    return(
+        <View style = {styles.home}>
+            <Text style = {styles.homeText}>
+                ¡Bienvenido!
+            </Text>
+        </View>
+    );
+};
+
+export default function App2() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000)
+    }, []);
+
+    if (loading) {
+        return <SplashScreen />;
+    }
+    return <HomeScreen />
+}
+
+const styles = StyleSheet.create({
+        
+    splash: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+        
+    home: {
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+        
+    logo: {
+        fontSize: 80
+    },
+
+    title: {
+        fontSize: 30
+    },
+
+    homeText: {
+        fontSize: 30
+    }
+});
